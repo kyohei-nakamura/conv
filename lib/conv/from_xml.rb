32ba234a3a5ddec96795b8221921e94a7977cd0a
kyohei-nakamura <nakamura.kyohei.lab@gmail.com>
2015-05-21 18:29:49 +0900

    CSV変換時にShiftJISで表現できない文字の対応について修正

--- lib/conv/from_xml.rb
+++ lib/conv/from_xml.rb
@@ -55,48 +55,48 @@ module Conv
     end
 
     def getCheckItem item, root, row, know_how_detail
-      row[CHECK_ITEM_NAME] = item.xpath('xmlns:CheckItemName').text
-      row[SEARCH_PROCESS] = item.xpath('xmlns:SearchProcess').text
+      row[CHECK_ITEM_NAME] = item.xpath('xmlns:CheckItemName').text.encode(Encoding::Shift_JIS, undef: :replace)
+      row[SEARCH_PROCESS] = item.xpath('xmlns:SearchProcess').text.encode(Encoding::Shift_JIS, undef: :replace)
       row[SEARCH_EXIST] = item.attr('searchExistance')
-      row[FACTOR] = item.xpath('xmlns:PortabilityFactor').text
-      row[DEGREE] = item.xpath('xmlns:PortabilityDegree').text
-      row[DEGREE_DETAIL] = item.xpath('xmlns:DegreeDetail').text
-      row[VISUAL_CONFIRM] = item.xpath('xmlns:VisualConfirm').text
-      row[HEARING_CONFIRM] = item.xpath('xmlns:HearingConfirm').text
+      row[FACTOR] = item.xpath('xmlns:PortabilityFactor').text.encode(Encoding::Shift_JIS, undef: :replace)
+      row[DEGREE] = item.xpath('xmlns:PortabilityDegree').text.encode(Encoding::Shift_JIS, undef: :replace)
+      row[DEGREE_DETAIL] = item.xpath('xmlns:DegreeDetail').text.encode(Encoding::Shift_JIS, undef: :replace)
+      row[VISUAL_CONFIRM] = item.xpath('xmlns:VisualConfirm').text.encode(Encoding::Shift_JIS, undef: :replace)
+      row[HEARING_CONFIRM] = item.xpath('xmlns:HearingConfirm').text.encode(Encoding::Shift_JIS, undef: :replace)
 
-      row[KNOWLEDGE_DETAIL] = item.xpath('xmlns:CheckItemNo').text == '1' ? know_how_detail : ''
+      row[KNOWLEDGE_DETAIL] = item.xpath('xmlns:CheckItemNo').text.encode(Encoding::Shift_JIS, undef: :replace) == '1' ? know_how_detail : ''
 
       search_info_ary = Array.new
       root.xpath("//xmlns:SearchInfomation[@searchInfoId='#{item.attr('searchRefKey')}']").each do |e|
-        row[FILE_TYPE] = e.xpath('xmlns:FileType').text
-        row[KEYWORD_1] = e.xpath('xmlns:SearchKey1').text
-        row[KEYWORD_2] = e.xpath('xmlns:SearchKey2').text
-        row[MODULE] = e.xpath('xmlns:PythonModule').text
-        row[LINE_NUM_APPROPRIATE] = e.xpath("xmlns:Appropriate").attr('lineNumberAppropriate').text
-        row[APPROPRIATE_CONTENTS] = e.xpath('xmlns:Appropriate/xmlns:AppropriateContents').text
-        line_no = e.xpath('xmlns:LineNumberInfomation/xmlns:LineNumber').text
+        row[FILE_TYPE] = e.xpath('xmlns:FileType').text.encode(Encoding::Shift_JIS, undef: :replace)
+        row[KEYWORD_1] = e.xpath('xmlns:SearchKey1').text.encode(Encoding::Shift_JIS, undef: :replace)
+        row[KEYWORD_2] = e.xpath('xmlns:SearchKey2').text.encode(Encoding::Shift_JIS, undef: :replace)
+        row[MODULE] = e.xpath('xmlns:PythonModule').text.encode(Encoding::Shift_JIS, undef: :replace)
+        row[LINE_NUM_APPROPRIATE] = e.xpath("xmlns:Appropriate").attr('lineNumberAppropriate').text.encode(Encoding::Shift_JIS, undef: :replace)
+        row[APPROPRIATE_CONTENTS] = e.xpath('xmlns:Appropriate/xmlns:AppropriateContents').text.encode(Encoding::Shift_JIS, undef: :replace)
+        line_no = e.xpath('xmlns:LineNumberInfomation/xmlns:LineNumber').text.encode(Encoding::Shift_JIS, undef: :replace)
         row[TODO] = line_no if ! integer_string? line_no
         row[LINE_NUM] = line_no if integer_string? line_no
-        row[LINE_NUM_CONTENTS] = e.xpath('xmlns:LineNumberInfomation/xmlns:LineNumberContents').text
-        row[INVEST] = e.xpath('xmlns:LineNumberInfomation/xmlns:Investigation').text
+        row[LINE_NUM_CONTENTS] = e.xpath('xmlns:LineNumberInfomation/xmlns:LineNumberContents').text.encode(Encoding::Shift_JIS, undef: :replace)
+        row[INVEST] = e.xpath('xmlns:LineNumberInfomation/xmlns:Investigation').text.encode(Encoding::Shift_JIS, undef: :replace)
       end
       row
     end
 
     def getEntryCategory out, root, node
-      cat_id = node.text
-      chap_name = root.xpath("//xmlns:ChapterList/xmlns:Chapter/xmlns:ChapterName[following-sibling::xmlns:ChildChapter/xmlns:ChapterCategoryRefKey/text()='#{cat_id}']").text
-      chap_no = root.xpath("//xmlns:ChildChapter/xmlns:ChildCapterNo[following-sibling::xmlns:ChapterCategoryRefKey/text()='#{cat_id}']").text
+      cat_id = node.text.encode(Encoding::Shift_JIS, undef: :replace)
+      chap_name = root.xpath("//xmlns:ChapterList/xmlns:Chapter/xmlns:ChapterName[following-sibling::xmlns:ChildChapter/xmlns:ChapterCategoryRefKey/text()='#{cat_id}']").text.encode(Encoding::Shift_JIS, undef: :replace)
+      chap_no = root.xpath("//xmlns:ChildChapter/xmlns:ChildCapterNo[following-sibling::xmlns:ChapterCategoryRefKey/text()='#{cat_id}']").text.encode(Encoding::Shift_JIS, undef: :replace)
 
       cat = root.xpath("//xmlns:CategoryList/xmlns:Category[@categoryId='#{cat_id}']")
-      cat_name = cat.xpath('xmlns:CategoryName/text()').text
+      cat_name = cat.xpath('xmlns:CategoryName/text()').text.encode(Encoding::Shift_JIS, undef: :replace)
 
-      par_cat_id = root.xpath("//xmlns:EntryCategoryRefKey[following-sibling::xmlns:ChildEntry/xmlns:EntryCategoryRefKey/text()='#{cat_id}']").text
-      par_name = root.xpath("//xmlns:CategoryList/xmlns:Category[@categoryId='#{par_cat_id}']/xmlns:CategoryName/text()").text
+      par_cat_id = root.xpath("//xmlns:EntryCategoryRefKey[following-sibling::xmlns:ChildEntry/xmlns:EntryCategoryRefKey/text()='#{cat_id}']").text.encode(Encoding::Shift_JIS, undef: :replace)
+      par_name = root.xpath("//xmlns:CategoryList/xmlns:Category[@categoryId='#{par_cat_id}']/xmlns:CategoryName/text()").text.encode(Encoding::Shift_JIS, undef: :replace)
 
       cat.xpath('xmlns:KnowhowRefKey').tap{|s| out << [chap_no, chap_name, cat_name, par_name] if s.empty?}.each do |know_how_ref|
-        know_how = root.xpath("//xmlns:KnowhowList/xmlns:KnowhowInfomation[@knowhowId='#{know_how_ref.text}']")
-        know_how_name = know_how.xpath('xmlns:KnowhowName').text
+        know_how = root.xpath("//xmlns:KnowhowList/xmlns:KnowhowInfomation[@knowhowId='#{know_how_ref.text.encode(Encoding::Shift_JIS, undef: :replace)}']")
+        know_how_name = know_how.xpath('xmlns:KnowhowName').text.encode(Encoding::Shift_JIS, undef: :replace)
         know_how_detail_ref = know_how.attribute('knowhowDetailRefKey') if ! know_how.empty?
         know_how_detail = root.xpath("//xmlns:DocBook[@articleId='#{know_how_detail_ref}']/ns3:article/ns3:section/node()").to_ary.map{|e| e.to_xml.strip}.join
 
@@ -112,7 +112,7 @@ module Conv
       Output.new(@argv).get_output do |out|
         file = Nokogiri::XML(open(@argv[:f]))
         file.tap{|s| s.xpath("//xmlns:EntryCategoryRefKey").each {|node| getEntryCategory out, s, node}}
-        out << ["@@" + file.xpath("//xmlns:PortabilityKnowhowTitle").text]
+        out << ["@@" + file.xpath("//xmlns:PortabilityKnowhowTitle").text.encode(Encoding::Shift_JIS, undef: :replace)]
       end
     end
   end
